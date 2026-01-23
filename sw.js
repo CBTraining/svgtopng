57615c58c7f007fa3fc8f54498427c04345cbb19
CB Training <Training@cb-retail.com>
2026-01-23 13:50:37 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
+// A basic service worker is required for PWA installation to work in Chrome.
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Installed');
+});
 
+self.addEventListener('fetch', (e) => {
+    // Basic pass-through fetch handler
+    e.respondWith(fetch(e.request));
+});
