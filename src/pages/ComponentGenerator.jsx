29b1183f93823e3818c7c60b38ad8128e605a671
XPISigma <xpisigma@gmail.com>
2026-07-08 12:45:35 -0400

    Remove export padding from Component Generator

--- src/pages/ComponentGenerator.jsx
+++ src/pages/ComponentGenerator.jsx
@@ -560,7 +560,7 @@ ${materialLink}<div class="glow-card">
             <style>{cssCode}</style>
             <div dangerouslySetInnerHTML={{ __html: materialLink }} />
             <div style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
-              <div ref={previewRef} style={{ padding: '50px', display: 'flex' }}>
+              <div ref={previewRef} style={{ display: 'flex' }}>
                 {iconLink.trim() ? (
                   <a href={iconLink} target="_blank" rel="noopener noreferrer" className="glow-card" dangerouslySetInnerHTML={{ __html: innerHtml }} style={{ textDecoration: 'none', color: 'inherit' }} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave} />
                 ) : (
